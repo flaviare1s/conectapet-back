f3fef24f750b408be3613d29495cfec7a400178a
Andre F. Moura <andre11moura@outlook.com>
2025-06-06 12:23:51 -0300

    fix: user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,8 +6,8 @@ export const userRouter = Router();
 userRouter.post("/", UserController.create);
 userRouter.get("/", UserController.getAll);
 userRouter.get("/:id", UserController.getById);
-userRouter.post('/request-verification', UserController.solicitarVerificacaoemail);
-userRouter.post('/verify-email', UserController.verificarEmail);
+userRouter.post('/request-verification', UserController.requestVerification);
+userRouter.post('/verify-email', UserController.verifyEmail);
 userRouter.put("/:id", UserController.update);
 userRouter.delete("/:id", UserController.delete);
 
