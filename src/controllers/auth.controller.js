90b12b69396e03d9fdadcd547e4b37a365560c5b
flaviare1s <flaviarster@gmail.com>
2025-06-16 15:54:11 -0300

    feat: refresh token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,8 +3,18 @@ import authService from "../services/auth.service.js";
 export async function loginController(req, res, next) {
   try {
     const { email, senha } = req.body;
-    const { token } = await authService.login(email, senha);
-    res.json({ token });
+    const tokens = await authService.login(email, senha);
+    res.json(tokens);
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function refreshTokenController(req, res, next) {
+  try {
+    const { refreshToken } = req.body;
+    const tokens = authService.refreshToken(refreshToken);
+    res.json(tokens);
   } catch (error) {
     next(error);
   }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { loginController } from "../controllers/auth.controller.js";
+import { loginController, refreshTokenController } from "../controllers/auth.controller.js";
 
 export const authRouter = Router();
 
 authRouter.post("/", loginController);
+authRouter.post("/refresh-token", refreshTokenController);
 
 
 export default authRouter;
